8a43c736fcd0ffe0509363a728eaf601b13e899a
Sharrod Hines <sharrodhines@gmail.com>
2021-12-30 14:00:16 -0500

    unsorted the availabe meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -32,20 +32,7 @@ const DUMMY_MEALS = [
   },
 ];
 
-const sortedMeals = DUMMY_MEALS.sort( function ( a, b ) {
-  let nameA = a.name.toLowerCase()
-  let nameB = b.name.toLowerCase()
 
-  if ( nameA < nameB ) { //sort string ascending
-    return -1
-  }
-
-  if ( nameA > nameB ) {
-    return 1
-  }
-
-  return 0 //default return value (no sorting)
-} )
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map( meal => (
