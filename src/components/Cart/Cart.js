6588f0c14c258fe89e08c586322470dda8e4c84b
Sharrod Hines <hines-sharrod@users.noreply.github.com>
2022-01-19 12:41:10 -0500

    updated order form logic and styling

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
@@ -7,39 +7,51 @@ import classes from "./Cart.module.css";
 import OrderForm from "./OrderForm";
 
 const Cart = (props) => {
-  const cartCtx = useContext(CartContext);
+	const cartCtx = useContext(CartContext);
+	const [orderForm, setOrderForm] = useState(false);
 
-  const elegantTotalAmount = "$" + parseFloat(cartCtx.totalAmount.toFixed(2));
+	const elegantTotalAmount = "$" + parseFloat(cartCtx.totalAmount.toFixed(2));
 
-  const cartList = cartCtx.items.map((item) => (
-    <CartItem
-      key={item.id}
-      name={item.name}
-      amount={item.amount}
-      price={item.price}
-      onRemove={() => cartCtx.removeItem(item.id)}
-      onAdd={() => cartCtx.addItem({ ...item, amount: 1 })}
-    />
-  ));
+	const cartList = cartCtx.items.map((item) => (
+		<CartItem
+			key={item.id}
+			name={item.name}
+			amount={item.amount}
+			price={item.price}
+			onRemove={() => cartCtx.removeItem(item.id)}
+			onAdd={() => cartCtx.addItem({ ...item, amount: 1 })}
+		/>
+	));
 
-  return (
-    <Modal onBackdropClose={props.onHideCart}>
-      {cartList}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>{elegantTotalAmount}</span>
-      </div>
-      <OrderForm />
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onHideCart}>
-          Close
-        </button>
-        {cartCtx.items.length > 0 && (
-          <button className={classes.button}>Order</button>
-        )}
-      </div>
-    </Modal>
-  );
+	const orderFormHandler = () => {
+		setOrderForm(true);
+	};
+
+	const cartActions = (
+		<div className={classes.actions}>
+			<button className={classes["button--alt"]} onClick={props.onHideCart}>
+				Close
+			</button>
+			{cartCtx.items.length > 0 && (
+				<button className={classes.button} onClick={orderFormHandler}>
+					Order
+				</button>
+			)}
+		</div>
+	);
+
+	return (
+		<Modal onBackdropClose={props.onHideCart}>
+			{cartList}
+			<div className={classes.total}>
+				<span>Total Amount</span>
+				<span>{elegantTotalAmount}</span>
+			</div>
+
+			{orderForm && <OrderForm hideCart={props.onHideCart} />}
+			{!orderForm && cartActions}
+		</Modal>
+	);
 };
 
 export default Cart;
--- src/components/Cart/OrderForm.js
+++ src/components/Cart/OrderForm.js
@@ -1,21 +1,78 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import classes from "./OrderForm.module.css";
 
+const isNotEmpty = (value) => value.trim() !== "";
+const isFiveChar = (value) => value.trim().length === 5;
+
 const OrderForm = (props) => {
+  const [formFieldsValidity, setFormFieldsValidity] = useState({
+    name: true,
+    address: true,
+    city: true,
+    zipcode: true
+  });
+
+  const nameRef = useRef();
+  const addressRef = useRef();
+  const cityRef = useRef();
+  const zipcodeRef = useRef();
+
+  const orderFormSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const orderFormInfo = {
+      name: nameRef.current.value,
+      address: addressRef.current.value,
+      city: cityRef.current.value,
+      zipcode: zipcodeRef.current.value
+    };
+
+    const { name, address, city, zipcode } = orderFormInfo;
+
+    const nameIsValid = isNotEmpty(name);
+    const addressIsValid = isNotEmpty(address);
+    const cityIsValid = isNotEmpty(city);
+    const zipcodeIsValid = isFiveChar(zipcode);
+
+    const formIsValid =
+      nameIsValid && addressIsValid && cityIsValid && zipcodeIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
+    console.log(name, address, city, zipcode);
+  };
+
   return (
-    <form>
-      <label htmlFor="name">Name</label>
-      <input type="text" id="name" />
+    <form onSubmit={orderFormSubmitHandler}>
+      <div className={classes.formGroup}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" ref={nameRef} />
+      </div>
 
-      <label htmlFor="address">Address</label>
-      <input type="text" id="address" />
+      <div className={classes.formGroup}>
+        <label htmlFor="address">Address</label>
+        <input type="text" id="address" ref={addressRef} />
+      </div>
 
-      <label htmlFor="city">City</label>
-      <input type="text" id="city" />
+      <div className={classes.formGroup}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityRef} />
+      </div>
 
-      <label htmlFor="zipcode">Zip Code</label>
-      <input type="text" id="zipcode" />
+      <div className={classes.formGroup}>
+        <label htmlFor="zipcode">Zip Code</label>
+        <input type="text" id="zipcode" ref={zipcodeRef} />
+      </div>
+
+      <div className={classes.actions}>
+        <button type="button" onClick={props.hideCart}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
